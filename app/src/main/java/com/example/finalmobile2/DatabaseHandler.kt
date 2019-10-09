80169888552cb695a96b8d311c87eae7ab87fd91
PC <earthyo.wtF@gmail.com>
2019-10-09 19:42:36 +0700

    update DatabaseHandler @7:42 pm 9/10/2019

--- app/src/main/java/com/example/finalmobile2/DatabaseHandler.kt
+++ app/src/main/java/com/example/finalmobile2/DatabaseHandler.kt
@@ -1,8 +1,10 @@
 package com.example.finalmobile2
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 class DatabaseHandler (context: Context):
         SQLiteOpenHelper(context, DB_NAME, null, DB_VERSIOM){
@@ -16,6 +18,16 @@ class DatabaseHandler (context: Context):
 
     }
 
+    fun addUser(user: Users): Boolean{
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(FIRST_NAME, user.firstName)
+        values.put(LAST_NAME, user.lastName)
+        val _success = db.insert(TABLE_NAME, null, values)
+        db.close()
+        Log.v("InsertedID", "$_success")
+        return (Integer.parseInt("$_success")!= -1)
+    }
 
 
 
